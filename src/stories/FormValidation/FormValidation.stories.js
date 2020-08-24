d6eb293ea864e5006d2f831e6bf8759fc8084f55
Ilya Chukanov <ichukanov@intellective.com>
2020-08-24 14:46:03 +0300

    CustomFormValidationComponent

--- src/stories/FormValidation/FormValidation.stories.js
+++ src/stories/FormValidation/FormValidation.stories.js
@@ -2,7 +2,14 @@ import React, {useEffect, useState} from 'react';
 
 import * as R from "ramda";
 
+import {useDispatch, useSelector} from "react-redux";
+
+import AppPage from "../../components/AppPage/AppPage";
+
 import {
+	CreateCaseAttachmentsPage,
+	CreateCasePreviewPage,
+	CreateCaseViewForm,
 	CreateCaseWithAttachments,
 	DefaultActionFactory,
 	DefaultActionMapper,
@@ -10,6 +17,9 @@ import {
 	DefaultCreateCaseSubmitHandler,
 	DefaultFormSubmitHandlerMapper,
 	GridView,
+	ResourceCreateViewTitle,
+	ResourceCreateWizard,
+	withActionView,
 	withFormFieldValidators,
 	withGridViewActionExecutor,
 	withGridViewConfigLoader,
@@ -21,7 +31,10 @@ import {
 	withGridViewSorting
 } from "@intellective/core";
 
-import AppPage from "../../components/AppPage/AppPage";
+import CreateIcon from "@material-ui/icons/Create";
+import Alert from "@material-ui/lab/Alert";
+import {updateFormFieldValidation} from "@intellective/core/build/store/actions/forms";
+import {Typography} from "@material-ui/core";
 
 export default {title: 'Examples/Form Validation'};
 
@@ -150,6 +163,105 @@ export const CustomFormSubmitHandler = () => {
 		]);
 
 		this.getFormSubmitHandler = (props) => DomainFormSubmitHandlerMapper(props) || DefaultFormSubmitHandlerMapper(props);
+	}
+
+	return (
+		<AppPage href="/api/1.0.0/config/perspectives/search/dashboards/page12"
+		         ActionFactory={DomainActionFactory}
+		         ComponentFactory={DomainComponentFactory}
+		/>
+	);
+};
+
+export const CustomFormValidationComponent = () => {
+
+	const DomainFormValidationStatus = props => {
+		const {formId} = props;
+
+		const dispatch = useDispatch();
+
+		// eslint-disable-next-line react-hooks/rules-of-hooks
+		const formSelector = useSelector(({forms}) => (forms[formId]));
+
+		const formState = (formSelector && formSelector.data) || {};
+
+		const taskIdValue = R.path(["task_id", "value"], formState);
+		const taskIdInvalid = R.path(["task_id", "invalid"], formState);
+		const taskIdErrorText = R.path(["task_id", "errorText"], formState);
+
+		useEffect(() => {
+			if (taskIdValue === undefined) {
+				return;
+			}
+
+			if (taskIdValue === null) {
+				dispatch(updateFormFieldValidation(formId, "task_id", false));
+			} else {
+				dispatch(updateFormFieldValidation(formId, "task_id", Number(taskIdValue) !== 0, "TaskId must be equal to 0"));
+			}
+		}, [taskIdValue]);
+
+		// Optional
+		if (Boolean(taskIdInvalid)) {
+			return (
+				<Alert severity="warning" role="alert">
+					<Typography variant="h6">{taskIdErrorText}</Typography>
+				</Alert>
+			);
+		}
+
+		return null;
+	}
+
+	const withDomainFormValidationStatus = R.curry((WrappedForm, props) => {
+		return (
+			<>
+				<DomainFormValidationStatus {...props}/>
+
+				<WrappedForm {...props}/>
+			</>
+		);
+	});
+
+	const DomainCreateCasePropertiesPage = {
+		label: 'Fill in details',
+		icon: CreateIcon,
+		view: R.compose(withDomainFormValidationStatus)(CreateCaseViewForm),
+		onSuccess: () => "Case successfully created"
+	};
+
+	const DomainCreateCaseAction = (props) => withActionView({
+		...props,
+		title: <ResourceCreateViewTitle resourceName="Case"/>,
+		ViewForm: ResourceCreateWizard,
+		pages: [DomainCreateCasePropertiesPage, CreateCasePreviewPage, CreateCaseAttachmentsPage],
+		submitLabel: 'Create Case'
+	}, () => null);
+
+	function DomainActionFactory(defaultSettings = {}) {
+		DefaultActionFactory.call(this, defaultSettings);
+
+		const DomainActionMapper = R.curry((settings = {}, action) => {
+			// Add custom actions creation logic here. For example,
+
+			const isCreateAction = R.propEq('type', 'create');
+			const isCaseResource = R.propEq('resourceName', "cases");
+			const isCreateCaseAction = R.allPass([isCreateAction, isCaseResource]);
+
+			return R.cond([
+				[isCreateCaseAction, R.always(DomainCreateCaseAction(settings))],
+			])(action);
+		});
+
+		this.createAction = R.curry((action, props) => {
+			const {settings = {}, ...otherProps} = props;
+
+			const {view: viewSettings = {}} = settings;
+
+			const ActionComponent = DomainActionMapper({...defaultSettings, ...viewSettings}, action) || DefaultActionMapper({...defaultSettings, ...viewSettings}, action);
+
+			return <ActionComponent {...otherProps} {...action} action={action}/>;
+		});
 	}
 
 	return (
