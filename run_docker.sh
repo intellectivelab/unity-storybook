2ec2de2d44c5288e0f45ea30052c7f4497ba1712
Oleg Zimakov <ozimakov@intellective.com>
2020-11-11 15:36:11 +0300

    Fix docker script

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker run --rm --name=storybook_api -v "$PWD":/usr/src/app -v "$HOME/.npmrc":"/usr/src/app/.npmrc" -w /usr/src/app -d -p 4001:4000 node:12-alpine npm install && yarn server
-docker run --rm --name=storybook -v "$PWD":/usr/src/app -v "$HOME/.npmrc":"/usr/src/app/.npmrc" -w /usr/src/app -d -p 4002:6006 node:12-alpine npm install && yarn storybook
+docker run --rm --name=storybook_api -v "$PWD":/usr/src/app -v "$HOME/.npmrc":"/usr/src/app/.npmrc" -w /usr/src/app -it -p 4001:4000 node:12-alpine npm install
+docker run --rm --name=storybook_api -v "$PWD":/usr/src/app -v "$HOME/.npmrc":"/usr/src/app/.npmrc" -w /usr/src/app -d -p 4001:4000 node:12-alpine yarn server
+docker run --rm --name=storybook -v "$PWD":/usr/src/app -v "$HOME/.npmrc":"/usr/src/app/.npmrc" -w /usr/src/app -d -p 4002:6006 node:12-alpine yarn storybook
