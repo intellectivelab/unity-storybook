b6513b5ad4a12d60406b565522a91e19abcedd07
vshmelev <vshmelev@intellective.com>
2020-11-20 16:43:46 +0300

    unity-api-mocks dependency

--- server/config/components.js
+++ server/config/components.js
@@ -1,15 +1,5 @@
-const users = require("./users");
-const usersAnalytics = require("./usersAnalytics");
 const caseTasks = require("./caseTasks");
-const caseTasksAnalytics = require("./caseTasksAnalytics");
-const documents = require("./documents");
-const attachments = require("./attachments");
 
 module.exports = {
-    ...users,
-    ...usersAnalytics,
     ...caseTasks,
-    ...caseTasksAnalytics,
-    ...documents,
-    ...attachments
 };
