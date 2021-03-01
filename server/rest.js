a7312ab58e8811dbee1475c53113943d26c856f3
Ilya Chukanov <ichukanov@intellective.com>
2021-03-01 21:02:11 +0300

    using 2021.1.0-alpha.42

--- server/rest.js
+++ server/rest.js
@@ -8,11 +8,9 @@ const config = R.mergeDeepRight(unityApiMocks.defaultConfig, domainConfig);
 
 const usersApi = require("./api/usersApi");
 
-const useCustomUserLinks = record => R.mergeDeepRight(record, {_links: {'custom.verify': {href: '/api/users/verify'}}});
-
 module.exports = function (app) {
 	unityApiMocks.configApi(app, config);
-	unityApiMocks.usersApi(app, config, undefined, useCustomUserLinks);
+	unityApiMocks.usersApi(app, config, undefined);
 	unityApiMocks.contentApi(app, config);
 	unityApiMocks.casesApi(app, config);
 	unityApiMocks.foldersApi(app, config);
