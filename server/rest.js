6ef19bfd18390cf2e4e7e8f5e842d261b6beeafc
Joel N. Walkley <jwalkley@intellective.com>
2021-06-29 08:53:10 -0400

    update foldersApi from mocks

--- server/rest.js
+++ server/rest.js
@@ -9,6 +9,8 @@ const config = R.mergeDeepRight(unityApiMocks.defaultConfig, domainConfig);
 const formsApi = require("./api/formsApi");
 const usersApi = require("./api/usersApi");
 
+const setupFolders = unityApiMocks.utils.folders;
+
 module.exports = function (app) {
 
 	const usersViewLinkTransformer = (user) => ({
@@ -16,11 +18,13 @@ module.exports = function (app) {
 		_links: {...user._links, view: {href: "/api/config/components/usersView0"}}
 	});
 
+	const folderEntries = setupFolders();
+
 	unityApiMocks.configApi(app, config);
 	unityApiMocks.usersApi(app, config, undefined, usersViewLinkTransformer);
 	unityApiMocks.contentApi(app, config);
 	unityApiMocks.casesApi(app, config);
-	unityApiMocks.foldersApi(app, config);
+	unityApiMocks.foldersApi(app, config, folderEntries);
 	unityApiMocks.selectorsApi(app, config);
 	formsApi(app);
 	usersApi(app);
